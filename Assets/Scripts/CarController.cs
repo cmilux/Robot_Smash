0cb9b919aa029c8ff2655c9ddb726b04dd926b1d
julid97 <julid_2010@hotmail.com.ar>
2026-04-10 22:37:26 -0300

    create player and basic movement

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CarController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float turnSpeed = 100f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        // Movimiento hacia adelante/atrás
+        float move = moveInput.y * speed * Time.fixedDeltaTime;
+        Vector3 movement = transform.forward * move;
+
+        rb.MovePosition(rb.position + movement);
+
+        // Rotación
+        float turn = moveInput.x * turnSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+}
+
