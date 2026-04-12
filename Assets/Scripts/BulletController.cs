1f6a01d8f25d29d33c46b827fe521251f824acea
julid97 <julid_2010@hotmail.com.ar>
2026-04-11 22:26:58 -0300

    Fix aim rotation and add basic enemy health system

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     public float speed = 20f;
+    public int damage = 1;
+
     private Rigidbody rb;
 
     void Start()
@@ -12,5 +14,16 @@ public class BulletController : MonoBehaviour
         rb.linearVelocity = transform.forward * speed;
 
         Destroy(gameObject, 2f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        EnemyHealth enemy = collision.gameObject.GetComponent<EnemyHealth>();
+
+        if (enemy != null) 
+        {
+            enemy.TakeDamage(damage); 
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/WeaponControllerAutoAim.cs
+++ Assets/Scripts/WeaponControllerAutoAim.cs
@@ -6,23 +6,31 @@ public class WeaponControllerAutoAim : MonoBehaviour
     public Transform aim;
     public Transform firePoint;
     public GameObject bulletPrefab;
+
+    private GameObject currentEnemy; 
+
     void Update()
     {
-        // Get all enemies in the scene and add to the array enemies 
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        
-        //If there are no enemies exit
-        if (enemies.Length == 0) return;
+        if (currentEnemy == null)
+        {
+            // Get all enemies in the scene and add to the array enemies 
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
-        //Pick a random enemy from the array
-        GameObject enemy = enemies[Random.Range(0, enemies.Length)];
+            // If there are no enemies exit
+            if (enemies.Length == 0) return;
 
-        //calculate direction towards the enemy
-        Vector3 direction = enemy.transform.position - aim.position;
-        direction.y = 0;
+            // Pick a random enemy from the array
+            currentEnemy = enemies[Random.Range(0, enemies.Length)];
+        }
+        if (currentEnemy != null)
+        {
+            // calculate direction towards the enemy
+            Vector3 direction = currentEnemy.transform.position - aim.position;
+            direction.y = 0;
 
-        //Rotate the aim towards the enemy (esto no esta funcionando bien, y no se soluciona poniendo un tiempo por enemigo, podria ser con un rango)
-        aim.LookAt(aim.position + direction);
+            // Rotate the aim towards the enemy
+            aim.LookAt(aim.position + direction);
+        }
     }
 
     public void OnAttack(InputValue value)
