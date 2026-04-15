667342aff0868103f118e5e0fc24b92d4ad6a755
Marti <villa12marti@gmail.com>
2026-04-15 01:23:23 -0300

    prototype enemy n3 (big enemy)

---
+++ Assets/Scripts/Enemies/BigEnemy.cs
@@ -0,0 +1,47 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Pool;
+
+public class BigEnemy : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject kamikazeEnemy;
+    public Transform player;
+    public Transform[] spawnPoint;
+    
+    public float shootingCooldown = 3f;
+    public float bulletTime;
+
+    public float currentEnemies;
+    float maxEnemies = 3;
+
+    private void Update()
+    {
+        agent.SetDestination(player.position);
+
+        ShootPlayer();
+    }
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void ShootPlayer()
+    {
+        while (currentEnemies < maxEnemies)
+        {
+            bulletTime -= Time.deltaTime;
+            if (bulletTime > 0) return;
+            bulletTime = shootingCooldown;
+
+            int spawnPointIndex = Random.Range(0, spawnPoint.Length);
+
+            Instantiate(kamikazeEnemy, spawnPoint[spawnPointIndex].position, Quaternion.identity);
+            currentEnemies++;
+
+            //solve how to reset counter
+        }
+    }
+}
--- Assets/Scripts/Enemies/KamikazeEnemy.cs
+++ Assets/Scripts/Enemies/KamikazeEnemy.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class KamikazeEnemy : MonoBehaviour
 {
-    EnemyHealth _enemyHealht;
+    //EnemyHealth _enemyHealht;
     public ParticleSystem _explosion;
 
     private void Start()
     {
-        _enemyHealht = GetComponent<EnemyHealth>();
+        //_enemyHealht = GetComponent<EnemyHealth>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        _explosion.Play();
-        //Destroy enemy if player collides w it
-        Destroy(gameObject, 5f);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _explosion.Play();
+            //Destroy enemy if player collides w it
+            Destroy(transform.root.gameObject, 5f);
+        }
     }
 }
