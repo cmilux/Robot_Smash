5695b1132e251dd99df5fd13d5f773cf020c6ec6
Marti <villa12marti@gmail.com>
2026-04-20 15:19:15 -0300

    kamikaze issue solved

--- Assets/Scripts/Enemies/KamikazeEnemy.cs
+++ Assets/Scripts/Enemies/KamikazeEnemy.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class KamikazeEnemy : Enemy
 {
-    public ParticleSystem _explosion;
-    public Transform player;
-    public float explodeDistance = 10f;
-    public int damage = 1;
-    bool kamIsDead = false;
+    public ParticleSystem _explosion;       //explosion particles
+    public Transform player;                //player transform's component
+    public float explodeDistance = 10f;     //distance to explode
+    public int damage = 1;                  //amount of damage caused by enemy
+    bool kamIsDead = false;                 //bool to check if enemy is dead
+    bool methodRun = false;                 //bool to check if distance method has run
 
     private void Start()
     {
@@ -20,7 +21,7 @@ public class KamikazeEnemy : Enemy
     }
 
 
-    //There is an issue w the distance attack, it kills the player at once
+    //need to find a way to cancel the attack when player moves away
     void Distance()
     {
         if (kamIsDead) return;
@@ -28,19 +29,19 @@ public class KamikazeEnemy : Enemy
         //Calculates the distance between player an enemy
         float distance = Vector3.Distance(transform.position, player.position);
 
-        if (distance <= explodeDistance)
+        if (distance <= explodeDistance && !methodRun)
         {
+            methodRun = true;
+
             Explode();
 
-            if (distance <= explodeDistance)
+            //Get the player health script
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
             {
-                //Get the player health script
-                PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
-                if (playerHealth != null)
-                {
-                    //Applies damage to player
-                    playerHealth.LoseHealth(damage);
-                }
+                //Applies damage to player
+                playerHealth.LoseHealth(damage);
+                Debug.Log("Player damage");
             }
         }
     }
@@ -52,6 +53,18 @@ public class KamikazeEnemy : Enemy
         _explosion.Play();
         timeBeforeDestroy = 5;      //Set time to be destroyed
         Die(timeBeforeDestroy);
+        kamIsDead = true;           //Enemy is dead now
+
+        if (!methodRun)
+        {
+            //Get the player health script
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                //Applies damage to player
+                playerHealth.LoseHealth(damage);
+            }
+        }
 
     }
 
@@ -61,18 +74,7 @@ public class KamikazeEnemy : Enemy
 
         if (collision.gameObject.CompareTag("Player"))
         {
-            //Get the player health script
-            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
-            if (playerHealth != null)
-            {
-                //Applies damage to player
-                playerHealth.LoseHealth(damage);
-            }
-
-            _explosion.Play();
-            //Destroy enemy if player collides w it
-            kamIsDead = true;
-            //Destroy(transform.root.gameObject, 5f);
+            Explode();
         }
     }
 }
