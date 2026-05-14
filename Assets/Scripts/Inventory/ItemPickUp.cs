8e2092e765744a8bc0f6b7af073b41a0bc6d31a7
julid97 <julid_2010@hotmail.com.ar>
2026-05-14 04:07:14 -0300

    Revert "Integrate networked item pickup and spawner system"
    This reverts commit ec2187250320ed80541eb1c0ec191fcd1be89d36.

--- Assets/Scripts/Inventory/ItemPickUp.cs
+++ Assets/Scripts/Inventory/ItemPickUp.cs
@@ -1,27 +1,7 @@
-using Unity.Netcode;
 using UnityEngine;
 
-public class ItemPickup : NetworkBehaviour
+public class ItemPickup : MonoBehaviour
 {
     public ItemData itemData;
     public int quantity = 1;
-
-   public void Pickup()
-    {
-        if (IsServer)
-        {
-            NetworkObject.Despawn();
-        }
-        else
-        {
-            PickupServerRpc();
-        }
-    }
-    
-    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
-    private void PickupServerRpc()
-    {
-        if (!IsServer) return;
-        NetworkObject.Despawn(true);
-    }
 }
