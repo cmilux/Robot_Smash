e158dd052246c67a51d11c2386fe33b4f154db77
julid97 <julid_2010@hotmail.com.ar>
2026-05-16 04:08:01 -0300

    Implement networked pickup and store inventory
    Falta corregir dropeo y equipamiento

--- Assets/Scripts/Inventory/ItemPickUp.cs
+++ Assets/Scripts/Inventory/ItemPickUp.cs
@@ -1,7 +1,37 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class ItemPickup : MonoBehaviour
+public class ItemPickup : NetworkBehaviour
 {
     public ItemData itemData;
+
     public int quantity = 1;
+
+    public void Pickup()
+    {
+        Debug.Log("PICKUP LLAMADO");
+        if (IsServer)
+        {
+            Debug.Log("Soy server");
+            NetworkObject.Despawn(false);
+        }
+        else
+        {
+            Debug.Log("SOy cliente");
+            PickupServerRpc();
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    private void PickupServerRpc()
+    {
+        NetworkObject.Despawn(false);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        gameObject.SetActive(false);
+
+        base.OnNetworkDespawn();
+    }
 }
