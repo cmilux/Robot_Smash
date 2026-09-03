2e1e8701f591d08518813e312a1ea64acd46d556
julid97 <julid_2010@hotmail.com.ar>
2026-09-03 03:19:23 -0300

    add loot boxes and barrels with configurable drops

--- Assets/Scripts/Inventory/LootBox.cs
+++ Assets/Scripts/Inventory/LootBox.cs
@@ -3,34 +3,69 @@ using UnityEngine;
 
 public class LootBox : NetworkBehaviour
 {
-    public ItemData itemToDrop;
+    //Template used to set items and amount in the Inspector
+    [System.Serializable]
+    public class LootOption
+    {
+        public ItemData item;
+        public int minAmount = 1;
+        public int maxAmount = 1;
+    }
+
+    //All the items this box could drop (set in the inspector)
+    public LootOption[] lootOptions;
+
+    //How many hits the box needs before breaking
+    public int hitsToBreak = 3;
+    private int hitsTaken = 0;
 
     private bool isBroken = false;
 
     private void OnCollisionEnter(Collision collision)
-    {   //only the serve can spawn items 
-        if (!IsServer) return; 
-
+    {
+        if (!IsServer) return;
         if (isBroken) return;
-
         if (!collision.gameObject.CompareTag("Player")) return;
 
-        BreakBox();
+        OnHit();
     }
 
-    private void BreakBox()
-    {   
-        isBroken = true;    
+    // Called every time the box gets hit//FIX!
+    private void OnHit()
+    {
+        hitsTaken++;
 
-        GameObject itemDrop = Instantiate(itemToDrop.dropPrefab, transform.position, Quaternion.identity);
+        if (hitsTaken >= hitsToBreak)
+        {
+            Break();
+        }
+    }
 
-        NetworkObject netObj = itemDrop.GetComponent<NetworkObject>();
+    //Breaks the box and drops one random item
+    private void Break()
+    {
+        isBroken = true;
 
+        //Pick one random option from the list
+        LootOption chosen = lootOptions[Random.Range(0, lootOptions.Length)];
+
+        //Pick a random amount between the min and max
+        int amount = Random.Range(chosen.minAmount, chosen.maxAmount + 1);
+
+        GameObject droppedItem = Instantiate(chosen.item.dropPrefab, transform.position, Quaternion.identity);
+
+        ItemPickup pickup = droppedItem.GetComponent<ItemPickup>();
+        if (pickup != null)
+        {
+            pickup.quantity = amount;
+        }
+
+        NetworkObject netObj = droppedItem.GetComponent<NetworkObject>();
         netObj.Spawn();
 
-        NetworkObject.Despawn(false);
+        QuestManager.Instance.ReportProgress(ObjectiveType.DestroyObject, chosen.item.id.ToString());
 
-        QuestManager.Instance.ReportProgress(ObjectiveType.DestroyObject, itemToDrop.id.ToString());
+        NetworkObject.Despawn(false);
     }
 
     public override void OnNetworkDespawn()
