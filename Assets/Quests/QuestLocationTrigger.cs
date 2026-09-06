8f44f6967023b3bb1350337a13e030902420ade2
Marti <villa12marti@gmail.com>
2026-09-06 16:07:38 -0300

    reach location issue fixed
    When playing multiplayer and colliding with a location trigger would count as 2 triggers cause the other player, by just watching, would also report the progress. This has been fixed

--- Assets/Quests/QuestLocationTrigger.cs
+++ Assets/Quests/QuestLocationTrigger.cs
@@ -1,3 +1,4 @@
+using Unity.Netcode;
 using UnityEngine;
 
 public class QuestLocationTrigger : MonoBehaviour               //UPDATES PROGRESS OF A MISSION
@@ -8,8 +9,13 @@ public class QuestLocationTrigger : MonoBehaviour               //UPDATES PROGRE
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player")) return;
         if (alreadyReported) return; // only report once — this isn't a counter, it's a one-time "arrived" event
+        if (!other.CompareTag("Player")) return;
+
+        //only client who owns the car reports (otherwise every client watching it happen, will report too)
+        //solo el cliente que es dueno del auto debe reportar (si no, cada auto que ve pasar el auto por el collider, tambien reporta el progreso)
+        NetworkObject netObj = other.GetComponentInParent<NetworkObject>();
+        if(netObj == null || !netObj.IsOwner) return;
 
         alreadyReported = true;
         QuestManager.Instance.ReportProgressServerRpc(ObjectiveType.ReachLocation, locationId, 1);
