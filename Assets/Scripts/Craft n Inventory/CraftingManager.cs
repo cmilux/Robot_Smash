187a5b85c83dac6d477fee9f360923a2bdc19934
julid97 <julid_2010@hotmail.com.ar>
2026-09-15 21:51:33 -0300

    delete resultslot crafted items go to inventory

--- Assets/Scripts/Craft n Inventory/CraftingManager.cs
+++ Assets/Scripts/Craft n Inventory/CraftingManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using Unity.Netcode;
 public class CraftingManager : MonoBehaviour
 {
     [Header("Crafting Slots")]
@@ -41,11 +42,20 @@ public class CraftingManager : MonoBehaviour
     {
         if (currentRecipe == null) return;
 
-        //Show the crafted item in the result slot
-        resultSlot.SetItem(currentRecipe.result, currentRecipe.resultQuantity);
+        Inventory playerInventory = GetLocalPlayerInventory();
 
-        //Remove the required ingredients from the crafting slots
-        ConsumeIngredients(currentRecipe);
+        if (playerInventory != null)
+        {
+            int leftover = playerInventory.AddItem(currentRecipe.result, currentRecipe.resultQuantity);
+
+            if(leftover < currentRecipe.resultQuantity)
+            {
+                //Remove the required ingredients from the crafting slots
+                ConsumeIngredients(currentRecipe);
+            }
+        }
+
+
     }
     private bool AllSlotsEmpty()
     {
@@ -146,5 +156,19 @@ public class CraftingManager : MonoBehaviour
                 }
             }
         }
+    }
+
+    private Inventory GetLocalPlayerInventory()
+    {
+        if (NetworkManager.Singleton != null && NetworkManager.Singleton.LocalClient != null)
+        {
+            var localPlayerObject = NetworkManager.Singleton.LocalClient.PlayerObject;
+
+            if (localPlayerObject != null)
+            {
+                return localPlayerObject.GetComponent<Inventory>();
+            }
+        }
+        return null;
     }
 }
